60662b83b874adf108581a5763d6b5eadc4364dc
Martin Larralde <martin.larralde@ens-cachan.fr>
2016-08-16 11:54:22 +0100

    Add usermeta option to CLI
    CLI now has a "-m" argument that can be either a path to a file
    containing a JSON formated usermeta dictionary, or directly a string
    containing the usermeta. If parsing fails, an UserWarning is issued
    and the parsing process will continue without any additional
    metadata.

--- nmrml2isa/parsing.py
+++ nmrml2isa/parsing.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from __future__ import print_function
 
-
 import sys
 import os
 import glob
@@ -50,7 +49,7 @@ def run():
     p.add_argument('-i', dest='in_dir', help='in folder containing mzML files', required=True)
     p.add_argument('-o', dest='out_dir', help='out folder, new directory will be created here', required=True)
     p.add_argument('-s', dest='study_name', help='study identifier name', required=True)
-    #p.add_argument('-n', dest='split', help='do NOT split assay files based on polarity', action='store_false', default=True)
+    p.add_argument('-m', dest='usermeta', help='additional user provided metadata (JSON format)', required=False)#, type=json.loads)
     p.add_argument('-v', dest='verbose', help='print more output', action='store_true', default=False)
     p.add_argument('-c', dest='process_count', help='number of processes to spawn (default: nbr of cpu * 4)',
                          action='store', default=None, type=int)
@@ -68,12 +67,23 @@ def run():
         print("out directory: {}".format(os.path.join(args.out_dir, args.study_name)))
         print("Sample identifier name:{}{}".format(args.study_name, os.linesep))
 
-    full_parse(args.in_dir, args.out_dir, args.study_name,
-               #args.usermeta if args.usermeta else {},
-               #args.split,
-               args.verbose, args.process_count)
+    try:
+        if not args.usermeta:
+            usermeta = None
+        elif os.path.isfile(args.usermeta):
+            with open(args.usermeta) as f:
+                usermeta = json.load(f)
+        else:
+            usermeta = json.loads(args.usermeta)
+    except json.decoder.JSONDecodeError:
+        warnings.warn("Usermeta could not be parsed.", UserWarning)
+        usermeta = None
 
-def full_parse(in_dir, out_dir, study_identifer, verbose=False, process_count=None):
+
+    full_parse(args.in_dir, args.out_dir, args.study_name,
+               usermeta,args.verbose, args.process_count)
+
+def full_parse(in_dir, out_dir, study_identifer, usermeta=None, verbose=False, process_count=None):
     """ Parses every study from *in_dir* and then creates ISA files.
 
     A new folder is created in the out directory bearing the name of
@@ -123,30 +133,12 @@ def full_parse(in_dir, out_dir, study_identifer, verbose=False, process_count=No
         #task = partial(parse_task, owl)
         metalist = [parse_task(owl, x, verbose) for x in nmrml_files]#pool.starmap(parse_task, ((owl, x, verbose) for x in nmrml_files))
 
-
-        #if not verbose:
-        #    pbar = pb.ProgressBar(widgets=['Parsing: ',
-        #                                   pb.Counter(), '/', str(len(nmrml_files)),
-        #                                   pb.Bar(marker="█", left=" |", right="| "),
-        #                                   pb.AdaptiveETA()])
-        #    for i in pbar(nmrml_files):
-        #        metalist.append(nmrml.nmrMLmeta(i, owl).meta)
-
-        #else:
-        #    for i in nmrml_files:
-        #        print("Parsing nmrML file: {}".format(i))
-        #        meta = nmrml.nmrMLmeta(i, owl).meta
-        #
-        #    metalist.append(meta)
-
-
-
         # update isa-tab file
         if metalist:
             print(''.join(['\r'*(not verbose),
             '[{}] Writing ISA-Tab files'.format(datetime.now().time().strftime('%H:%M:%S')),
             ' '*30]), end='\n'*verbose)
-            isa_tab_create = isa.ISA_Tab(out_dir, study_identifer).write(metalist)
+            isa_tab_create = isa.ISA_Tab(out_dir, study_identifer, usermeta).write(metalist)
 
             print(''.join(['\r'*(not verbose),
             '[{}] Finished writing ISA-Tab files'.format(datetime.now().time().strftime('%H:%M:%S'), out_dir),
