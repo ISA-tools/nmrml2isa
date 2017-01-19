4cddb2bc3962a52c4d0083b4b2ea4c9d7bf1ba26
tomnl <thomas.nigel.lawson@gmail.com>
2017-01-19 09:59:12 +0000

    small change to activate appveyor

--- scripts/for_pyinstaller/nmrml2isa_cli.py
+++ scripts/for_pyinstaller/nmrml2isa_cli.py
@@ -5,7 +5,6 @@ import pronto
 if __name__ == '__main__':
     parsing.main()
 
-
     # this tmp list is so for automated checking
     # of code sees that we are using the imports
     # really they are just required for pyinstaller
